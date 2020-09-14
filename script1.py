3844ffa38f8e81b06991ca250c9f55bd4780abc3
Vivan <vivanvatsa@gmail.com>
2020-09-14 22:15:35 +0530

    Update script1.py

--- script1.py
+++ script1.py
@@ -5,5 +5,14 @@ from tkinter import *
 # form init of tk() before the end of mainloop()
 window = Tk()
 
+b1 = Button(window, text="Execute")
+# b1.pack()
+b1.grid(row=0, column=0)
+
+e1 = Entry(window)
+e1.grid(row=0, column=1)
+
+t1 = Text(window)
+t1.grid(row=0, column=2)
 
 window.mainloop()
