b7ba0c3cfd5719384df5b76539473e06c5218290
Vivan <vivanvatsa@gmail.com>
2020-09-14 21:56:30 +0530

    Create script1.py

---
+++ script1.py
@@ -0,0 +1,9 @@
+# import tkinter
+from tkinter import *
+
+# you create all the required business logic and widgets inside this two main entity functions
+# form init of tk() before the end of mainloop()
+window = Tk()
+
+
+window.mainloop()
